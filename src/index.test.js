2db2e85234ac673bcf6c55f3e74ef60e4f8a9660
vivmagarwal <vivmagarwal@gmail.com>
2022-11-15 17:46:22 +0530

    class1 assignments added.

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,33 @@
-import {add} from './index';
+import {add, greet, interpolate, nuller, objectFactory} from './index';
 
-describe('Dummy test', function(){
+describe('add', function(){
   test('add two numbers', function(){
     expect(add(1,2)).toBe(3)
+  })
+})
+
+describe('greet', function(){
+  test('greet returns interpolated string', function(){
+    expect(greet()).toBe(`Will Smith's age is 54.`)
+  })
+})
+
+describe('interpolate', function(){
+  test('inputs are interpolated', function(){
+    expect(interpolate(2,3)).toBe(`2 + 3 = 5`);
+    expect(interpolate(5,2)).toBe(`5 + 2 = 7`);
+  })
+})
+
+describe('nuller', function(){
+  test('nuller returns null', function(){
+    expect(nuller()).toBe(null);
+  })
+})
+
+describe('objectFactory', function(){
+  test('objectFactory', function(){
+    expect(objectFactory('Raj', 26)).toMatchObject({Raj: 26});
+    expect(objectFactory('Vijay', 24)).toMatchObject({Vijay: 24});
   })
 })
